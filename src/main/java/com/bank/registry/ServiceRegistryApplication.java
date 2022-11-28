4728f1e0635ef19bc616975689eb5f7b708c895b
BICH <abdelali.bich-ext@galian.fr>
2022-11-28 01:24:58 +0100

    second commit

---
+++ src/main/java/com/bank/registry/ServiceRegistryApplication.java
@@ -0,0 +1,32 @@
+package com.bank.registry;
+
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
+@EnableEurekaServer
+@EnableDiscoveryClient
+@EnableAdminServer
+
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()
+					.and().csrf().disable();
+		}
+	}
+}
